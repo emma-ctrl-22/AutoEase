a0ad6a40285460490a8196b66f88992db215977e
emma-ctrl-22 <enyatepe.ac@st.vvu.edu.gh>
2024-09-16 11:14:57 +0000

    Add AuthProvider to App component

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import AppNavigator from './navigation/AppNavigator';
+import { AuthProvider } from './context/AuthContext';
 
 export default function App() {
   return (
+    <AuthProvider>
    <AppNavigator />
+    </AuthProvider>
   );
 }
 
